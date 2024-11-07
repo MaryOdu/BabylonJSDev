183a7f344ebe898df7cdcc37615382b1bc0a925d
MaryOdu <146565129+MaryOdu@users.noreply.github.com>
2024-11-07 14:40:01 +0000

    village

---
+++ babylonProj/village/src/createRunScene.ts
@@ -0,0 +1,8 @@
+import {} from "@babylonjs/core";
+
+import { SceneData } from "./interfaces";
+
+export default function createRunScene(runScene: SceneData) {
+ 
+  runScene.scene.onAfterRenderObservable.add(() => {});
+}
